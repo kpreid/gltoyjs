cfc2a32441d889bec380a00d23f9b8210756dca9
Kevin Reid <kpreid@switchb.org>
2012-06-27 23:05:09 -0400

    Remove vignetting from voronoi effect.
    (It was an unrelated experiment.)

--- effects/voronoi/vertex.glsl
+++ effects/voronoi/vertex.glsl
@@ -5,10 +5,8 @@ uniform mat4 uPMatrix;
 uniform vec4 uColor;
 
 varying vec4 vColor;
-varying vec4 vP;
 
 void main(void) {
-  vP = uMVMatrix * vec4(aVertexPosition, 1.0);
   gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
   vColor = uColor;
 }
