c7a969b1763f99fb77f7934e6961aed92bfece22
Kevin Reid <kpreid@switchb.org>
2012-06-30 14:56:38 -0400

    Effect specular: Normalize interpolated normals.

--- effects/specular/fragment.glsl
+++ effects/specular/fragment.glsl
@@ -10,11 +10,14 @@ varying vec3 vColor;
 
 void main(void) {
   vec3 color = vec3(0.0);
-
+  
+  // interpolation across surface does not preserve unit length
+  vec3 normal = normalize(vNormal);
+  
   for (int i = 0; i < NLIGHTS; i++) {
     color += uLightColor[i] *
-      pow(max(0.0, dot(vec3(0.0, 0.0, 1.0), reflect(-uLightDir[i], vNormal))), float(SHININESS));
+      pow(max(0.0, dot(vec3(0.0, 0.0, 1.0), reflect(-uLightDir[i], normal))), float(SHININESS));
   }
-
+  
   gl_FragColor = vec4(color, 1.0);
 }
